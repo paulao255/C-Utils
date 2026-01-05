11c16d153bf0ca61e294e896e1bdba22a4444d43
Grace <contactpaulao@gmail.com>
2026-01-05 11:12:25 -0300

    Update cutils.h in main branch.

--- cutils.h
+++ cutils.h
@@ -12,6 +12,7 @@
 		#define __USE_MINGW_ANSI_STDIO 1
 	#endif
 #endif
+
 #if !defined(_WIN32) || !defined(_WIN64) /* For anything else Windows. */
 	#ifndef _POSIX_C_SOURCE
 		#define _POSIX_C_SOURCE 199309L
@@ -27,7 +28,7 @@
 	#include <windows.h>
 	#include <direct.h>
 	#define MKDIR(dir) _mkdir(dir)
-#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) || defined(__DJGPP__) /* For Linux, Android, Apple and MS-DOS. */
+#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) /* For Linux, Android and Apple. */
 	#include <unistd.h>
 	#include <time.h>
 	#include <errno.h>
@@ -38,6 +39,9 @@
 	#include <unistd.h>
 	#include <time.h>
 	#include <errno.h>
+	#include <sys/stat.h>
+	#include <sys/types.h>
+	#define MKDIR(dir) mkdir(dir)
 #endif
 
 
@@ -47,40 +51,40 @@ extern "C"
 #endif
 
 /* C Utils version variables: */
-#define C_UTILS_FULL_VERSION 153.0L           /* C Utils full version variable (1.5.3).           */
-#define C_UTILS_MAJOR_VERSION  1.0L           /* C Utils major version variable (1).              */
-#define C_UTILS_MINOR_VERSION  5.0L           /* C Utils minor version variable (5).              */
-#define C_UTILS_PATCH_VERSION  3.0L           /* C Utils patch version variable (3).              */
+#define C_UTILS_FULL_VERSION  160.0L           /* C Utils full version variable (1.6.0).           */
+#define C_UTILS_MAJOR_VERSION   1.0L           /* C Utils major version variable (1).              */
+#define C_UTILS_MINOR_VERSION   6.0L           /* C Utils minor version variable (6).              */
+#define C_UTILS_PATCH_VERSION   0.0L           /* C Utils patch version variable (0).              */
 
 /* Terminal colors: */
-#define BASE_TERMINAL         "\033[m"        /* Reset terminal text.                             */
-#define BOLD                  "\033[1m"       /* Bold terminal text.                              */
-#define BASE_TERMINAL_BOLD    "\033[m\033[1m" /* Reset and bold terminal text.                    */
-#define ITALIC                "\033[3m"       /* Italic terminal text.                            */
-#define UNDERLINE             "\033[4m"       /* Underline terminal text.                         */
-#define BLACK_COLOR           "\033[30m"      /* Black terminal text.                             */
-#define RED_COLOR             "\033[31m"      /* Red terminal text.                               */
-#define GREEN_COLOR           "\033[32m"      /* Green terminal text.                             */
-#define YELLOW_COLOR          "\033[33m"      /* Yellow terminal text.                            */
-#define BLUE_COLOR            "\033[34m"      /* Blue terminal text.                              */
-#define MAGENTA_COLOR         "\033[35m"      /* Magenta terminal text.                           */
-#define CYAN_COLOR            "\033[36m"      /* Cyan terminal text.                              */
-#define WHITE_COLOR           "\033[37m"      /* White terminal text.                             */
+#define BASE_TERMINAL          "\033[m"        /* Reset terminal text.                             */
+#define BOLD                   "\033[1m"       /* Bold terminal text.                              */
+#define BASE_TERMINAL_BOLD     "\033[m\033[1m" /* Reset and bold terminal text.                    */
+#define ITALIC                 "\033[3m"       /* Italic terminal text.                            */
+#define UNDERLINE              "\033[4m"       /* Underline terminal text.                         */
+#define BLACK_COLOR            "\033[30m"      /* Black terminal text.                             */
+#define RED_COLOR              "\033[31m"      /* Red terminal text.                               */
+#define GREEN_COLOR            "\033[32m"      /* Green terminal text.                             */
+#define YELLOW_COLOR           "\033[33m"      /* Yellow terminal text.                            */
+#define BLUE_COLOR             "\033[34m"      /* Blue terminal text.                              */
+#define MAGENTA_COLOR          "\033[35m"      /* Magenta terminal text.                           */
+#define CYAN_COLOR             "\033[36m"      /* Cyan terminal text.                              */
+#define WHITE_COLOR            "\033[37m"      /* White terminal text.                             */
 
 /* Precision variables: */
-#define MIN_RPV                0              /* Minimum recommended precision value variable.    */
-#define MAX_RPV               18              /* Maximum recommended precision value variable.    */
+#define MIN_RPV                 0              /* Minimum recommended precision value variable.    */
+#define MAX_RPV                18              /* Maximum recommended precision value variable.    */
 
 /* Temperature limits variables: */
-#define MIN_CELSIUS_F       -273.15f          /* Minimum Celsius temperature (in float).          */
-#define MIN_CELSIUS_D       -273.15           /* Minimum Celsius temperature (in double).         */
-#define MIN_CELSIUS_L       -273.15L          /* Minimum Celsius temperature (in long double).    */
-#define MIN_FAHRENHEIT_F    -459.67f          /* Minimum Fahrenheit temperature (in float).       */
-#define MIN_FAHRENHEIT_D    -459.67           /* Minimum Fahrenheit temperature (in double).      */
-#define MIN_FAHRENHEIT_L    -459.67L          /* Minimum Fahrenheit temperature (in long double). */
-#define MIN_KELVIN_F           0.0f           /* Minimum Kelvin temperature (in float).           */
-#define MIN_KELVIN_D           0.0            /* Minimum Kelvin temperature (in double).          */
-#define MIN_KELVIN_L           0.0L           /* Minimum Kelvin temperature (in long double).     */
+#define MIN_CELSIUS_F        -273.15f          /* Minimum Celsius temperature (in float).          */
+#define MIN_CELSIUS_D        -273.15           /* Minimum Celsius temperature (in double).         */
+#define MIN_CELSIUS_L        -273.15L          /* Minimum Celsius temperature (in long double).    */
+#define MIN_FAHRENHEIT_F     -459.67f          /* Minimum Fahrenheit temperature (in float).       */
+#define MIN_FAHRENHEIT_D     -459.67           /* Minimum Fahrenheit temperature (in double).      */
+#define MIN_FAHRENHEIT_L     -459.67L          /* Minimum Fahrenheit temperature (in long double). */
+#define MIN_KELVIN_F            0.0f           /* Minimum Kelvin temperature (in float).           */
+#define MIN_KELVIN_D            0.0            /* Minimum Kelvin temperature (in double).          */
+#define MIN_KELVIN_L            0.0L           /* Minimum Kelvin temperature (in long double).     */
 
 /* Math utils: */
 #ifdef IMPORT_MATH_UTILS
@@ -175,8 +179,6 @@ static void enable_vt_and_utf8()
 			DWORD err = GetLastError();
 			(void)err;
 		}
-	#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) || defined(__DJGPP__) /* For Linux, Android, macOS and MS-DOS. */
-		return;
 	#endif
 }
 
@@ -225,29 +227,30 @@ static void ssleep(unsigned int time)
 		return;
 	}
 
-	#if defined(_WIN32) || defined(_WIN64) /* For Windows. */
-		Sleep((DWORD)time * (DWORD)1000UL);
-	#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) /* For Linux, Android and macOS. */
-		struct timespec req;
-		struct timespec rem;
-		int res;
+	#ifndef __DJGPP__ /* For any OS else MS-DOS. */
+		#if defined(_WIN32) || defined(_WIN64) /* For Windows. */
+			Sleep((DWORD)time * (DWORD)1000UL);
+		#else /* For Linux, Android and Apple. */
+			struct timespec req;
+			struct timespec rem;
+			int res;
 
-		req.tv_sec = (time_t)time;
-		req.tv_nsec = 0L;
+			req.tv_sec = (time_t)time;
+			req.tv_nsec = 0L;
 
-		while((res = nanosleep(&req, &rem)) == -1)
-		{
-			if(errno == EINTR)
+			while((res = nanosleep(&req, &rem)) == -1)
 			{
-				req = rem;
+				if(errno == EINTR)
+				{
+					req = rem;
+				}
+
+				else
+				{
+					break;
+				}
 			}
-			else
-			{
-				break;
-			}
-		}
-	#elif defined(__DJGPP__) /* For MS-DOS. */
-		return;
+		#endif
 	#endif
 }
 
@@ -259,29 +262,30 @@ static void mssleep(unsigned int time)
 		return;
 	}
 
-	#if defined(_WIN32) || defined(_WIN64) /* For Windows. */
-		Sleep((DWORD)time);
-	#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) /* For Linux, Android and macOS. */
-		struct timespec req;
-		struct timespec rem;
-		int res;
+	#ifndef __DJGPP__ /* For any OS else MS-DOS. */
+		#if defined(_WIN32) || defined(_WIN64) /* For Windows. */
+			Sleep((DWORD)time);
+		#else /* For Linux, Android and Apple. */
+			struct timespec req;
+			struct timespec rem;
+			int res;
 
-		req.tv_sec = (time_t)(time / 1000);
-		req.tv_nsec = (long)((time % 1000) * 1000000L);
+			req.tv_sec = (time_t)(time / 1000);
+			req.tv_nsec = (long)((time % 1000) * 1000000L);
 
-		while((res = nanosleep(&req, &rem)) == -1)
-		{
-			if(errno == EINTR)
+			while((res = nanosleep(&req, &rem)) == -1)
 			{
-				req = rem;
+				if(errno == EINTR)
+				{
+					req = rem;
+				}
+
+				else
+				{
+					break;
+				}
 			}
-			else
-			{
-				break;
-			}
-		}
-	#elif defined(__DJGPP__) /* For MS-DOS. */	
-		return;
+		#endif
 	#endif
 }
 
@@ -293,8 +297,7 @@ static void rrmf()
 		petc();
 		system("more /C /P README.md");
 		petc_a();
-		CLEAR_TERMINAL();
-	#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) /* For Linux, Android and macOS. */
+	#elif defined(__linux__) || defined(__ANDROID__) || defined(__APPLE__) /* For Linux, Android and Apple. */
 		puts("When you enter press \"q\" to quit, \"enter\" to go down to the next line, \"space\" to go down next page, and type \"/ + text\" to search for text!");
 		petc();
 		system("more -cp README.md");
@@ -306,9 +309,7 @@ static void rrmf()
 /* Open URL function: */
 static void url_openner(const char *url)
 {
-	#if defined(__DJGPP__) /* For MS-DOS. */
-		return;
-	#else /* For another OS. */
+	#ifndef __DJGPP__ /* For any OS else MS-DOS. */
 		if(url == NULL)
 		{
 			return;
@@ -322,7 +323,7 @@ static void url_openner(const char *url)
 				snprintf(command, sizeof(command), "start %s", url);
 			#elif defined(__linux__) || defined(__ANDROID__) /* For Linux and Android. */
 				snprintf(command, sizeof(command), "xdg-open %s", url);
-			#elif defined(__APPLE__) /* For macOS. */
+			#elif defined(__APPLE__) /* For Apple. */
 				snprintf(command, sizeof(command), "open %s", url);
 			#endif
 
@@ -337,15 +338,15 @@ static void easter_egg_function()
 	/* Basic commands: */
 	puts("Congratulations!!! You just discovered a new easter egg! (please don't say it to anywhone ok!)");
 	puts("This is the link to our github account! If you want to see our projects, codes, etc...");
-	puts("Link: https://github.com/GodProgrammer512/");
+	puts("Link: https://github.com/paulao255");
 
 	/* Command to open the link on browser: */
 	#if defined(_WIN32) || defined(_WIN64) /* For Windows. */
-	  system("start https://github.com/GodProgrammer512/");
+	  system("start https://github.com/paulao255");
 	#elif defined(__linux__) || defined(__ANDROID__) /* For Linux and Android. */
-	  system("xdg-open https://github.com/GodProgrammer512/");
+	  system("xdg-open https://github.com/paulao255");
 	#elif defined(__APPLE__) /* For macOS. */
-	  system("open https://github.com/GodProgrammer512/");
+	  system("open https://github.com/paulao255");
 	#endif
 
 	/* Pause APP: */
