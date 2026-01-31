e1ca79c61e2c2715dd0e5eccfb3ba695ddc154a0
paulao255 <contactpaulao@gmail.com>
2026-01-31 11:51:28 -0300

    Update include/C-Utils/cutils.h in main branch (2026-01-31).

--- include/C-Utils/cutils.h
+++ include/C-Utils/cutils.h
@@ -277,7 +277,7 @@ static struct tm current_time(void);                                            
 
 static struct tm current_time(void)
 {
-	return *localtime((long int) time(NULL));
+	return *localtime((const long int *) time(NULL));
 }
 
 /* Main functions prototypes: */
