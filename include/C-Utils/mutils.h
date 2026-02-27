166fb2f6d7e769940c53f536309ecc8a2cabad32
paulao255 <contactpaulao@gmail.com>
2026-02-27 14:25:08 -0300

    Update C-Utils to 2026-02-27 in main branch;
    Modified include/C-Utils/defs.h, include/C-Utils/mutils.h and include/C-Utils/tcutils.h;

--- include/C-Utils/mutils.h
+++ include/C-Utils/mutils.h
@@ -11,57 +11,57 @@ extern "C"
 #endif
 
 /* Math utils functions prototypes: */
-static const float circumfer_f(const float radius_val);                  /* Function to calculate a circumference using a float value.              */
-static const float circle_area_f(const float radius_val);                /* Function to calculate the area of a circle using a float value.         */
-static const float sphere_volume_f(const float radius_val);              /* Function to calculate the volume of a sphere using a float value.       */
-static const double circumfer_d(const double radius_val);                /* Function to calculate a circumference using a double value.             */
-static const double circle_area_d(const double radius_val);              /* Function to calculate the area of a circle using a double value.        */
-static const double sphere_volume_d(const double radius_val);            /* Function to calculate the volume of a sphere using a double value.      */
-static const long double circumfer_ld(const long double radius_val);     /* Function to calculate a circumference using a long double value.        */
-static const long double circle_area_ld(const long double radius_val);   /* Function to calculate the area of a circle using a long double value.   */
-static const long double sphere_volume_ld(const long double radius_val); /* Function to calculate the volume of a sphere using a long double value. */
+static float circumfer_f(const float radius_val);                  /* Function to calculate a circumference using a float value.              */
+static float circle_area_f(const float radius_val);                /* Function to calculate the area of a circle using a float value.         */
+static float sphere_volume_f(const float radius_val);              /* Function to calculate the volume of a sphere using a float value.       */
+static double circumfer_d(const double radius_val);                /* Function to calculate a circumference using a double value.             */
+static double circle_area_d(const double radius_val);              /* Function to calculate the area of a circle using a double value.        */
+static double sphere_volume_d(const double radius_val);            /* Function to calculate the volume of a sphere using a double value.      */
+static long double circumfer_ld(const long double radius_val);     /* Function to calculate a circumference using a long double value.        */
+static long double circle_area_ld(const long double radius_val);   /* Function to calculate the area of a circle using a long double value.   */
+static long double sphere_volume_ld(const long double radius_val); /* Function to calculate the volume of a sphere using a long double value. */
 
-static const float circumfer_f(const float radius_val)
+static float circumfer_f(const float radius_val)
 {
 	return 2.0f * PI_F * radius_val;
 }
 
-static const float circle_area_f(const float radius_val)
+static float circle_area_f(const float radius_val)
 {
 	return PI_F * (radius_val * radius_val);
 }
 
-static const float sphere_volume_f(const float radius_val)
+static float sphere_volume_f(const float radius_val)
 {
 	return (4.0f / 3.0f) * PI_F * (radius_val * radius_val * radius_val);
 }
 
-static const double circumfer_d(const double radius_val)
+static double circumfer_d(const double radius_val)
 {
 	return 2.0 * PI_D * radius_val;
 }
 
-static const double circle_area_d(const double radius_val)
+static double circle_area_d(const double radius_val)
 {
 	return PI_D * (radius_val * radius_val);
 }
 
-static const double sphere_volume_d(const double radius_val)
+static double sphere_volume_d(const double radius_val)
 {
 	return (4.0 / 3.0) * PI_D * (radius_val * radius_val * radius_val);
 }
 
-static const long double circumfer_ld(const long double radius_val)
+static long double circumfer_ld(const long double radius_val)
 {
 	return 2.0L * PI_LD * radius_val;
 }
 
-static const long double circle_area_ld(const long double radius_val)
+static long double circle_area_ld(const long double radius_val)
 {
 	return PI_LD * (radius_val * radius_val);
 }
 
-static const long double sphere_volume_ld(const long double radius_val)
+static long double sphere_volume_ld(const long double radius_val)
 {
 	return (4.0L / 3.0L) * PI_LD * (radius_val * radius_val * radius_val);
 }
--- include/C-Utils/tcutils.h
+++ include/C-Utils/tcutils.h
@@ -11,111 +11,111 @@ extern "C"
 #endif
 
 /* Temperature conversion functions prototypes: */
-static const float kelvin_to_celsius_f(const float kelvin_val);                      /* Function to convert Kelvin to Celsius using a float value.           */
-static const float kelvin_to_fahrenheit_f(const float kelvin_val);                   /* Function to convert Kelvin to Fahrenheit using a float value.        */
-static const float celsius_to_kelvin_f(const float celsius_val);                     /* Function to convert Celsius to Kelvin using a float value.           */
-static const float celsius_to_fahrenheit_f(const float celsius_val);                 /* Function to convert Celsius to Fahrenheit using a float value.       */
-static const float fahrenheit_to_kelvin_f(const float fahrenheit_val);               /* Function to convert Fahrenheit to Kelvin using a float value.        */
-static const float fahrenheit_to_celsius_f(const float fahrenheit_val);              /* Function to convert Fahrenheit to Celsius using a float value.       */
-static const double kelvin_to_celsius_d(const double kelvin_val);                    /* Function to convert Kelvin to Celsius using a double value.          */
-static const double kelvin_to_fahrenheit_d(const double kelvin_val);                 /* Function to convert Kelvin to Fahrenheit using a double value.       */
-static const double celsius_to_kelvin_d(const double celsius_val);                   /* Function to convert Celsius to Kelvin using a double value.          */
-static const double celsius_to_fahrenheit_d(const double celsius_val);               /* Function to convert Celsius to Fahrenheit using a double value.      */
-static const double fahrenheit_to_kelvin_d(const double fahrenheit_val);             /* Function to convert Fahrenheit to Kelvin using a double value.       */
-static const double fahrenheit_to_celsius_d(const double fahrenheit_val);            /* Function to convert Fahrenheit to Celsius using a double value.      */
-static const long double kelvin_to_celsius_ld(const long double kelvin_val);         /* Function to convert Kelvin to Celsius using a long double value.     */
-static const long double kelvin_to_fahrenheit_ld(const long double kelvin_val);      /* Function to convert Kelvin to Fahrenheit using a long double value.  */
-static const long double celsius_to_kelvin_ld(const long double celsius_val);        /* Function to convert Celsius to Kelvin using a long double value.     */
-static const long double celsius_to_fahrenheit_ld(const long double celsius_val);    /* Function to convert Celsius to Fahrenheit using a long double value. */
-static const long double fahrenheit_to_kelvin_ld(const long double fahrenheit_val);  /* Function to convert Fahrenheit to Kelvin using a long double value.  */
-static const long double fahrenheit_to_celsius_ld(const long double fahrenheit_val); /* Function to convert Fahrenheit to Celsius using a long double value. */
+static float kelvin_to_celsius_f(const float kelvin_val);                      /* Function to convert Kelvin to Celsius using a float value.           */
+static float kelvin_to_fahrenheit_f(const float kelvin_val);                   /* Function to convert Kelvin to Fahrenheit using a float value.        */
+static float celsius_to_kelvin_f(const float celsius_val);                     /* Function to convert Celsius to Kelvin using a float value.           */
+static float celsius_to_fahrenheit_f(const float celsius_val);                 /* Function to convert Celsius to Fahrenheit using a float value.       */
+static float fahrenheit_to_kelvin_f(const float fahrenheit_val);               /* Function to convert Fahrenheit to Kelvin using a float value.        */
+static float fahrenheit_to_celsius_f(const float fahrenheit_val);              /* Function to convert Fahrenheit to Celsius using a float value.       */
+static double kelvin_to_celsius_d(const double kelvin_val);                    /* Function to convert Kelvin to Celsius using a double value.          */
+static double kelvin_to_fahrenheit_d(const double kelvin_val);                 /* Function to convert Kelvin to Fahrenheit using a double value.       */
+static double celsius_to_kelvin_d(const double celsius_val);                   /* Function to convert Celsius to Kelvin using a double value.          */
+static double celsius_to_fahrenheit_d(const double celsius_val);               /* Function to convert Celsius to Fahrenheit using a double value.      */
+static double fahrenheit_to_kelvin_d(const double fahrenheit_val);             /* Function to convert Fahrenheit to Kelvin using a double value.       */
+static double fahrenheit_to_celsius_d(const double fahrenheit_val);            /* Function to convert Fahrenheit to Celsius using a double value.      */
+static long double kelvin_to_celsius_ld(const long double kelvin_val);         /* Function to convert Kelvin to Celsius using a long double value.     */
+static long double kelvin_to_fahrenheit_ld(const long double kelvin_val);      /* Function to convert Kelvin to Fahrenheit using a long double value.  */
+static long double celsius_to_kelvin_ld(const long double celsius_val);        /* Function to convert Celsius to Kelvin using a long double value.     */
+static long double celsius_to_fahrenheit_ld(const long double celsius_val);    /* Function to convert Celsius to Fahrenheit using a long double value. */
+static long double fahrenheit_to_kelvin_ld(const long double fahrenheit_val);  /* Function to convert Fahrenheit to Kelvin using a long double value.  */
+static long double fahrenheit_to_celsius_ld(const long double fahrenheit_val); /* Function to convert Fahrenheit to Celsius using a long double value. */
 
-static const float kelvin_to_celsius_f(const float kelvin_val)
+static float kelvin_to_celsius_f(const float kelvin_val)
 {
 	return kelvin_val - 273.15f;
 }
 
-static const float kelvin_to_fahrenheit_f(const float kelvin_val)
+static float kelvin_to_fahrenheit_f(const float kelvin_val)
 {
 	return kelvin_val * (9.0f / 5.0f) - 459.67f;
 }
 
-static const float celsius_to_kelvin_f(const float celsius_val)
+static float celsius_to_kelvin_f(const float celsius_val)
 {
 	return celsius_val + 273.15f;
 }
 
-static const float celsius_to_fahrenheit_f(const float celsius_val)
+static float celsius_to_fahrenheit_f(const float celsius_val)
 {
 	return celsius_val * (9.0f / 5.0f) + 32.0f;
 }
 
-static const float fahrenheit_to_kelvin_f(const float fahrenheit_val)
+static float fahrenheit_to_kelvin_f(const float fahrenheit_val)
 {
 	return (fahrenheit_val + 459.67f) * (5.0f / 9.0f);
 }
 
-static const float fahrenheit_to_celsius_f(const float fahrenheit_val)
+static float fahrenheit_to_celsius_f(const float fahrenheit_val)
 {
 	return (fahrenheit_val - 32.0f) * (5.0f / 9.0f);
 }
 
-static const double kelvin_to_celsius_d(const double kelvin_val)
+static double kelvin_to_celsius_d(const double kelvin_val)
 {
 	return kelvin_val - 273.15;
 }
 
-static const double kelvin_to_fahrenheit_d(const double kelvin_val)
+static double kelvin_to_fahrenheit_d(const double kelvin_val)
 {
 	return kelvin_val * (9.0 / 5.0) - 459.67;
 }
 
-static const double celsius_to_kelvin_d(const double celsius_val)
+static double celsius_to_kelvin_d(const double celsius_val)
 {
 	return celsius_val + 273.15;
 }
 
-static const double celsius_to_fahrenheit_d(const double celsius_val)
+static double celsius_to_fahrenheit_d(const double celsius_val)
 {
 	return celsius_val * (9.0 / 5.0) + 32.0;
 }
 
-static const double fahrenheit_to_kelvin_d(const double fahrenheit_val)
+static double fahrenheit_to_kelvin_d(const double fahrenheit_val)
 {
 	return (fahrenheit_val + 459.67) * (5.0 / 9.0);
 }
 
-static const double fahrenheit_to_celsius_d(const double fahrenheit_val)
+static double fahrenheit_to_celsius_d(const double fahrenheit_val)
 {
 	return (fahrenheit_val - 32.0) * (5.0 / 9.0);
 }
 
-static const long double kelvin_to_celsius_ld(const long double kelvin_val)
+static long double kelvin_to_celsius_ld(const long double kelvin_val)
 {
 	return kelvin_val - 273.15L;
 }
 
-static const long double kelvin_to_fahrenheit_ld(const long double kelvin_val)
+static long double kelvin_to_fahrenheit_ld(const long double kelvin_val)
 {
 	return kelvin_val * (9.0L / 5.0L) - 459.67L;
 }
 
-static const long double celsius_to_kelvin_ld(const long double celsius_val)
+static long double celsius_to_kelvin_ld(const long double celsius_val)
 {
 	return celsius_val + 273.15L;
 }
 
-static const long double celsius_to_fahrenheit_ld(const long double celsius_val)
+static long double celsius_to_fahrenheit_ld(const long double celsius_val)
 {
 	return celsius_val * (9.0L / 5.0L) + 32.0L;
 }
 
-static const long double fahrenheit_to_kelvin_ld(const long double fahrenheit_val)
+static long double fahrenheit_to_kelvin_ld(const long double fahrenheit_val)
 {
 	return (fahrenheit_val + 459.67L) * (5.0L / 9.0L);
 }
 
-static const long double fahrenheit_to_celsius_ld(const long double fahrenheit_val)
+static long double fahrenheit_to_celsius_ld(const long double fahrenheit_val)
 {
 	return (fahrenheit_val - 32.0L) * (5.0L / 9.0L);
 }
